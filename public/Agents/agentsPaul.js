474a9b529ac6feab259fe0c45b17f5c23deefb1b
Paul_Coulm <paulcoulm@gmail.com>
2025-02-14 14:36:06 +0100

    Ajout de l'agent Commande_Pompe()

--- public/Agents/agentsPaul.js
+++ public/Agents/agentsPaul.js
@@ -1,8 +1,9 @@
 import {Template} from '../Template.js'
+import {Tuple} from "../Tuple.js";
 
-let etatPompe = "off"
+let etatPompe = "off";
 
-export async function AgentPompe(ts) {
+export async function Pompe(ts) {
     // Templates pour les valeurs de la pompe
     let tempActivPompe = new Template(["activation_pompe"]);
     let tempDesactivPompe = new Template(["desactivation_pompe"]);
@@ -10,15 +11,35 @@ export async function AgentPompe(ts) {
     let valActive = ts.inp(tempActivPompe);
     let valDesac = ts.inp(tempDesactivPompe);
 
-    console.log("AGENT POMPE ACTIF")
-
     // activation ou desactivation de la pompe
-    if(valActive){
-        console.log("activation de la pompe");
+    if(valActive != null){
+        console.log("Agent_Pompe : Activation de la pompe");
         etatPompe = "on"
     }
-    if(valDesac){
-        console.log("desactivation de la pompe");
+    if(valDesac != null){
+        console.log("Agent_Pompe : Desactivation de la pompe");
         etatPompe = "off"
+    }
+}
+
+export async function Commande_Pompe(ts, seuil_CH4, seuil_CO){
+    let tempH20_haut_detecte = new Template(["H20_haut_detecte"]);
+    const H20_haut_detecte = await ts.in(tempH20_haut_detecte);     // valeur detection eau haut
+
+    let tempNiveau_CH4 = new Template(["niveau_CH4"]);
+    const y = await ts.rd(tempNiveau_CH4);      // valeur du niveau de gaz CH4
+
+    let tempNiveau_CO = new Template(["niveau_CO"]);
+    const z = await ts.rd(tempNiveau_CO);        // valeur du niveau de CO
+
+    if(y.values[1] < seuil_CH4 && z.values[1] < seuil_CO){
+        ts.out(new Tuple(["activation_pompe"]));
+        ts.out(new Tuple(["detection_H20_bas"]));
+        ts.out(new Tuple(["detection_gaz_haut"]));
+    }
+    if(y.values[1] >= seuil_CH4 || z.values[1] >= seuil_CO){
+        console.log("Agent Commande_Pompe : Activation du ventilateur");
+        ts.out(new Tuple(["activation_ventilateur"]));
+        ts.out(new Tuple(["detection_gaz_bas"]));
     }
 }
